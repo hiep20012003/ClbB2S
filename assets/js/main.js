392e65535dd550c2ab24f1db084d6961192c1c16
hiep20012003 <nguyendunghiep20012003@gmail.com>
2024-03-04 15:14:31 +0700

    Add new project landing page clb

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+// NAV MENU BEHAVIOR
+
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+// show nav menu
+if (navMenu) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    });
+}
+
+// hidden nav menu
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+//  remove nav menu when clicked nav link
+const navLinks = document.querySelectorAll('.nav-link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+
+    navMenu.classList.remove('show-menu');
+}
+
+navLinks.forEach(n => n.addEventListener('click', linkAction));
+
+
